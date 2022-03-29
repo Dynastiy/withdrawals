dfa83ad8a6cc11af9a13ce56180268d3c0c73c06
Dynastiy <emmanuel00michael@gmail.com>
2022-03-29 19:22:09 +0100

    fixed other pages

--- src/views/pages/declinedTransactions.vue
+++ src/views/pages/declinedTransactions.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
-    <h1>Declined Withdrawal requests</h1>
+    <h1>All Withdrawal requests</h1>
     <table class="rwd-table">
       <tr>
-        <th>Reference</th>
-        <th>Full Name</th>
+        <th>Id</th>
+        <th>Merchant</th>
         <th>Amount</th>
         <th>Date</th>
         <th>Status</th>
         <th>Actions</th>
       </tr>
-      
-      <tr>
-        <td data-th="Reference">FE13423528</td>
-        <td data-th="Full Name">John Doe</td>
-        <td data-th="Amount">0.002szc</td>
-        <td data-th="Date">27th March 2022</td>
-        <td data-th="Status"><span class="declined">Declined</span></td>
-        <td data-th="Actions"> <px-button><span>View More</span></px-button> </td>
+      <tr v-if="withdrawals.length == 0">
+        <td class="text-danger font-weight-bold">
+          No Declined Withdrawal requests yet
+        </td>
       </tr>
-     
-      <tr>
-        <td data-th="Reference">FE13423528</td>
-        <td data-th="Full Name">John Doe</td>
-        <td data-th="Amount">0.002szc</td>
-        <td data-th="Date">27th March 2022</td>
-        <td data-th="Status"><span class="declined">Declined</span></td>
+      <tr v-for="withdrawal in withdrawals" :key="withdrawal.id">
+        <td data-th="id" >   </td>
+        <td data-th="Merchant" v-if="withdrawal.merchant"> {{ withdrawal.merchant.business_name }} </td>
+        <td data-th="Amount"> {{ withdrawal.amount }}{{ withdrawal.currency }} </td>
+        <td data-th="Date"> {{withdrawal.created_at}} </td>
+        <td data-th="Status"><span :class="[withdrawal.status]">{{withdrawal.status}}</span></td>
         <td data-th="Actions"> <px-button><span>View More</span></px-button> </td>
       </tr>
     </table>
@@ -37,6 +32,25 @@ import PxButton from '@/components/pxButton.vue'
 export default {
     components: {
         PxButton
+    },
+    data:()=>{
+      return{
+        withdrawals: []
+      }
+    },
+    methods: {
+      async getWithdrawals(){
+       try {
+          let res = await this.$axios.get('/merchants/admin/withdrawal-requests');
+        console.log(res.data.results);
+        this.withdrawals = res.data.results
+       } catch (error) {
+         console.log(error);
+       }
+      }
+    },
+    async created(){
+      this.getWithdrawals();
     }
 }
 </script>
